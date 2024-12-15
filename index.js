559f88535fe02e9e1b384a04afd54b301d527c50
Awaneesh Soni <awaneesh.soni.cer21@iitbhu.ac.in>
2024-12-15 10:20:38 +0530

    rename to index

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ mongoose.connect(process.env.MONGO_URI);
 console.log("monogp");
 
 app.use("/api/courses", courseRoutes);
+app.get('/', (req,res)=>{
+    res.send("Working")
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
